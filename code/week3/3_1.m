56b4cb9500e920a740d98dd62e0e5d3fd0743bb2
Ryota Eisaki <ryotaeisaki0530@icloud.com>
2020-10-29 19:47:11 +0900

    edit

---
+++ code/week3/3_1.m
@@ -0,0 +1,31 @@
+load n_4
+
+n = 4;
+
+x = zeros(n*n, 1); % 初期状態の設定
+
+maxit = 2000; % 最大反復回数
+tol = 1e-04; % 判定条件
+
+r = b - A*x;
+p = r;
+
+for i = 1:maxit
+    v = 0;
+    Ap = A*p;
+    alpha = dot(p,r)/dot(p,Ap);
+    x = x + alpha*p;
+    r = r - alpha*Ap;
+    beta = -dot(r,Ap)/dot(p,Ap);
+    p = r + beta*p;
+    v = norm(r);
+    t(i) = v;
+    t2(i) = i;
+    if v < tol
+        break;
+    end
+end
+
+plot(t);
+
+
