0929e4125add1e9c86387e5e5ac27957f115eae4
Ryota Eisaki <ryotaeisaki0530@icloud.com>
2020-12-17 15:40:49 +0900

    x_k

--- code/week8/least_squares.m
+++ code/week8/least_squares.m
@@ -2,4 +2,39 @@ load("data_MNIST.mat");
 m = 784;
 n = 10000;
 
-% imshow(reshape(X(:,1), [28, 28]))
+%%%%%%%%% X_kを求める
+tol = 1e-14;
+[U,S,V] = svd(X);
+s = svd(X);
+s_max = s(1);
+
+for i =1:m
+    val = s(i)/s_max;
+    if val < tol
+        i = i-1;
+        break;
+    end
+end
+
+k = i;
+
+for j=1:k
+    U_k(:,j)=U(:,j);
+end
+
+for j=1:k
+    V_k(:,j)=V(:,j);
+end
+
+for j=1:k
+    sk(j)=s(j);
+end
+
+sig_k = diag(sk);
+
+tmp = sig_k\transpose(U_k);
+
+X_k = V_k *tmp;
+
+%%%%%%%%% X_kを求める
+
